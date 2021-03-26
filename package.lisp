48d993b43850c8c17fe28ae1944221c31fd3db06
chip2n <andreas@arvidsson.io>
2021-03-26 06:55:31 +0100

    Reexport some arrow macros

--- package.lisp
+++ package.lisp
@@ -1,8 +1,10 @@
 (uiop:define-package #:chiputils
   (:nicknames #:c)
-  (:use #:cl #:alexandria #:yason)
+  (:use #:cl #:alexandria #:yason #:arrow-macros)
   (:export #:read-json-file
            #:sassoc
            #:group
            #:dump-output
-           #:with-interned-symbols))
+           #:with-interned-symbols
+           #:->
+           #:->>))
